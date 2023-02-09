26141faa67c6b6d15a6f4516b6a7883e541bd029
ariellezcano <ariellezcano1995@gmail.com>
2023-02-09 13:51:16 -0300

    ...

--- controlSistemas/src/app/pages/pages-routing.module.ts
+++ controlSistemas/src/app/pages/pages-routing.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LstEstadoComponent } from './lst/lst-estado/lst-estado.component';
 import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
@@ -12,19 +13,19 @@ const routes: Routes = [
     //     path: '',
     //     //component: PantallaPrincipalComponent,
     //   },
-    //   {
-    //     path: 'lst-departamento',
-    //     children: [
-    //       {
-    //         path: 'abm/:id',
-    //         component: AbmDepartamentoComponent,
-    //       },
-    //       {
-    //         path: '',
-    //         component: LstDepartamentoComponent,
-    //       },
-    //     ],
-    //   },
+      {
+        path: 'lst-estados',
+        children: [
+          // {
+          //   path: 'abm/:id',
+          //   component: AbmDepartamentoComponent,
+          // },
+          {
+            path: '',
+            component: LstEstadoComponent,
+          },
+        ],
+      },
       
     ],
   },
